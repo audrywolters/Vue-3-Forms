9221d50ef962f9dc249ce9ec8c69d1f870a3687c
Audry <audry@greyedsolutions.com>
2022-05-09 12:22:38 -0500

    created BaseRadioGroup. replaced BaseRadio with it to SimpleForm.

--- src/views/SimpleForm.vue
+++ src/views/SimpleForm.vue
@@ -33,20 +33,10 @@
 
       <h3>Are pets allowed?</h3>
       <div>
-        <BaseRadio
+        <BaseRadioGroup
           v-model="event.pets"
-          :value="1"
-          label="Yes"
           name="pets"
-        />
-      </div>
-      
-      <div>
-        <BaseRadio
-          v-model="event.pets"
-          :value="0"
-          label="No"
-          name="pets"
+          :options="petOptions"
         />
       </div>
 
@@ -75,6 +65,7 @@ import BaseInput from "../components/BaseInput.vue"
 import BaseSelect from "../components/BaseSelect.vue";
 import BaseCheckbox from "../components/BaseCheckbox.vue";
 import BaseRadio from "../components/BaseRadio.vue";
+import BaseRadioGroup from "../components/BaseRadioGroup.vue";
 export default {
     data() {
         return {
@@ -97,9 +88,13 @@ export default {
                     catering: false,
                     music: false
                 }
-            }
+            },
+            petOptions: [
+              { label: 'Yes', value: 1 },
+              { label: 'No', value: 0 }
+            ]
         };
     },
-    components: { BaseInput, BaseSelect, BaseCheckbox, BaseRadio }
+    components: { BaseInput, BaseSelect, BaseCheckbox, BaseRadio, BaseRadioGroup }
 }
 </script>
